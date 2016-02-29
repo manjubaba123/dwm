48c90fa749bd8b371f4fb887ed905b7519ffdd91
manjubaba123 <manuel.erdoes@bluewin.ch>
2016-02-29 17:23:51 +0100

    Added files via upload

--- dmenu/config.h
+++ dmenu/config.h
@@ -4,13 +4,13 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"-*-terminus-medium-*-*-*-16-*-*-*-*-*-*-*"
+	"-*-terminus-medium-r-*-*-16-*-*-*-*-*-*-*"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the elft of input field */
 static const char *normbgcolor = "#142D36"; /* -nb option; normal background                 */
-static const char *normfgcolor = "#8fc3d6"; /* -nf option; normal foreground                 */
-static const char *selbgcolor  = "#005577"; /* -sb option; selected background               */
-static const char *selfgcolor  = "#eeeeee"; /* -sf option; selected foreground               */
+static const char *normfgcolor = "#b9c9ce"; /* -nf option; normal foreground                 */
+static const char *selbgcolor  = "#142D36"; /* -sb option; selected background               */
+static const char *selfgcolor  = "#66e0ff"; /* -sf option; selected foreground               */
 static const char *outbgcolor  = "#00ffff";
 static const char *outfgcolor  = "#000000";
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
