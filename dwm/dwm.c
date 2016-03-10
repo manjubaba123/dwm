4e8ce9c4ccc3eda1d51892895f6ef77769539005
manjubaba123 <manuel.erdoes@bluewin.ch>
2016-03-10 17:43:40 +0100

    test

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -1,6 +1,6 @@
 /* See LICENSE file for copyright and license details.
  *
- * dynamic window manager is designed like any other X client as well. It is
+ * dynamico window manager is designed like any other X client as well. It is
  * driven through handling X events. In contrast to other X clients, a window
  * manager selects for SubstructureRedirectMask on the root window, to receive
  * events about window (dis-)appearance.  Only one X connection at a time is
